22eec38bf591b9259653c7ceb7b970cdeb904809
csjhonathan <jhonathancarv.s@gmail.com>
2023-05-31 21:54:32 -0300

    feat: finalized the signIn process saving the token in localStorage the user data in a context for future use

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SignUpPage from './pages/SignUpPage.jsx';
 import ResetStyles from './styles/resetStyles.js';
 import GlobalStyles from './styles/globalStyles.js';
+import SignInPage from './pages/SignInPage.jsx';
+import TimelinePage from './pages/TimelinePage.jsx';
+import { UserProvider } from './context/userContext.jsx';
 
 function App() {
   return (
     <BrowserRouter>
+
       <ResetStyles />
       <GlobalStyles />
-      <Routes>
-        <Route path="/" element={<SignUpPage />} />
-      </Routes>
+
+      <UserProvider>
+        <Routes>
+          <Route path="/" element={<SignInPage />} />
+          <Route path="/sign-up" element={<SignUpPage />} />
+          <Route path="/timeline" element={<TimelinePage />} />
+        </Routes>
+      </UserProvider>
+
     </BrowserRouter>
   );
 }
---
+++ src/pages/SignInPage.jsx
@@ -0,0 +1,74 @@
+/* eslint-disable react/jsx-props-no-spreading, no-unused-vars, no-restricted-syntax */
+
+import axios from 'axios';
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
+import { useContext, useState } from 'react';
+import SideLogo from '../components/SideLogo.jsx';
+import {
+  SignInContainer,
+  SignInForm,
+  Input,
+  SignInButton,
+  SwitchButton,
+} from '../styles/signInPage.styles.js';
+import { UserContext } from '../context/userContext.jsx';
+
+export default function SignInPage() {
+  const { register, handleSubmit } = useForm();
+  const navigate = useNavigate();
+  const [disabled, setDisabled] = useState(false);
+  const { setUserData } = useContext(UserContext);
+  async function submitForm(form) {
+    setDisabled(true);
+    const keys = Object.keys(form);
+
+    for (const key of keys) {
+      if (!form[key]) {
+        alert(`The ${key} field is blank!`);
+        return setDisabled(false);
+      }
+    }
+
+    try {
+      const response = await axios.post(`${process.env.REACT_APP_API_URL}/signin`, form);
+      localStorage.setItem('linkr_token', response.data.token);
+      setUserData(response.data.userData);
+      setDisabled(false);
+      return navigate('/timeline');
+    } catch (error) {
+      setDisabled(false);
+      if (error?.response.status === 401) return alert(error.response.data.message);
+
+      if (error?.response.status === 422) {
+        return alert(error.response.data.message.join(' '));
+      }
+
+      if (error?.response.status === 404) return alert(error.response.data.message);
+
+      return alert('There was an unexpected error!');
+    }
+  }
+  return (
+    <SignInContainer>
+      <SideLogo />
+      <SignInForm onSubmit={handleSubmit(submitForm)}>
+
+        <Input
+          type="text"
+          placeholder="e-mail"
+          disabled={disabled}
+          {...register('email')}
+        />
+        <Input
+          type="password"
+          placeholder="password"
+          disabled={disabled}
+          {...register('password')}
+        />
+        <SignInButton disabled={disabled}>Log In</SignInButton>
+        <SwitchButton disabled={disabled} onClick={() => navigate('/sign-up')} type="button">First time? Create an account!</SwitchButton>
+      </SignInForm>
+    </SignInContainer>
+  );
+}
