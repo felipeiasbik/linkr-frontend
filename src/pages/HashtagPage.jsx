7c888f723a1f93c4de6eb07f519a5644da74f42e
Felipe Pires Iasbik <felipeiasbik@hotmail.com>
2023-06-02 18:29:57 -0300

    feat: hashtag screen

--- src/pages/HashtagPage.jsx
+++ src/pages/HashtagPage.jsx
@@ -1,18 +1,83 @@
 /* eslint-disable react/jsx-props-no-spreading, no-unused-vars, no-restricted-syntax */
-
-import styled from 'styled-components';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import reactStringReplace from 'react-string-replace';
 import Sidebar from '../components/Sidebar.jsx';
+import {
+  LinkIds, Container, Title, SubContainer, Main, SideBar, Posts, InfoLeft, InfoRight, Articles,
+} from '../styles/hashtagStyles.js';
 
 export default function HashtagPage() {
+  const { hashtag } = useParams();
+  const navigate = useNavigate();
+  const [listPosts, setListPosts] = useState([]);
+  const [clickedLink, setClickedLink] = useState('');
+
+  useEffect(() => {
+    const token = localStorage.getItem('linkr_token');
+    if (!token) {
+      navigate('/');
+    } else {
+      const config = {
+        headers: { Authorization: `Bearer ${token}` },
+      };
+      axios.get(`${process.env.REACT_APP_API_URL}/trending/${hashtag}`, config)
+        .then((res) => {
+          const trendings = res.data;
+          setListPosts(trendings);
+        })
+        .catch((err) => {
+          alert(err.response.data);
+        });
+    }
+  }, [hashtag, clickedLink]);
+
+  const handleLinkClick = (clickedHashtag) => {
+    setClickedLink(clickedHashtag);
+  };
+
   return (
-    <Center>
-      <Sidebar />
-    </Center>
+    <Container>
+      <Title data-test="hashtag-title">
+        #
+        {' '}
+        {hashtag}
+      </Title>
+      <SubContainer>
+        <Main>
+          {listPosts?.map(({
+            postId, url, description, name, photo,
+          }) => (
+            <Posts data-test="post" key={postId}>
+              <InfoLeft>
+                <img alt={name} src={photo} />
+              </InfoLeft>
+              <InfoRight>
+                <h2>{name}</h2>
+                <p>
+                  {reactStringReplace(description, /(#\w+)/g, (match, i) => (
+                    <LinkIds
+                      to={`/hashtag/${match.slice(1)}`}
+                      key={i}
+                      onClick={() => handleLinkClick(match)}
+                      style={{ color: '#FFFFFF' }}
+                    >
+                      {match}
+                    </LinkIds>
+                  ))}
+                </p>
+                <Articles>
+                  {url}
+                </Articles>
+              </InfoRight>
+            </Posts>
+          ))}
+        </Main>
+        <SideBar>
+          <Sidebar />
+        </SideBar>
+      </SubContainer>
+    </Container>
   );
 }
-const Center = styled.div`
-margin-top: 30px;
-display: flex;
-justify-content: center;
-align-items: center;
-`;
---
+++ src/styles/hashtagStyles.js
@@ -0,0 +1,126 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import colors from '../constants/colors.js';
+import fonts from '../constants/fonts.js';
+
+export const LinkIds = styled(Link)`
+    text-decoration: none;
+    color: #ffffff;
+`;
+export const Container = styled.div`
+padding-top: 53px;
+background-color: ${colors.main};
+width: 936px;
+min-height:100%;
+min-height: 100vh;
+display: flex;
+flex-direction: column;
+align-items: flex-start;
+margin: 0 auto;
+@media (max-width: 768px){
+padding-top: 10px;
+width: 100%;
+  }
+`;
+export const Title = styled.h1`
+color: ${colors.neutral};
+font-family: ${fonts.secondary};
+font-size: 43px;
+font-weight: 700;
+display: flex;
+justify-content: flex-start;
+padding-bottom: 41px;
+@media (max-width: 768px){
+padding-left: 20px;
+padding-bottom: 30px;
+  }
+`;
+export const SubContainer = styled.div`
+width: 936px;
+display: flex;
+justify-content: space-between;
+@media (max-width: 768px){
+width: 100%;
+  }
+`;
+export const Main = styled.div`
+width: 611px;
+@media (max-width: 768px){
+width: 100%;
+  }
+`;
+export const SideBar = styled.div`
+display: flex;
+align-items: flex-start;
+@media (max-width: 768px){
+    display: none;
+}
+`;
+export const Posts = styled.div`
+background-color: ${colors.modal};
+border-radius: 16px;
+display: flex;
+margin-bottom: 16px;
+@media (max-width: 768px){
+    border-radius: 0px;
+    padding-right: 15px;
+  }
+`;
+export const InfoLeft = styled.div`
+width: 54px;
+display: flex;
+flex-direction: column;
+padding: 17px;
+img {
+    width: 50px;
+    height: 50px;
+    object-fit: cover;
+    border-radius: 50%;
+}
+@media (max-width: 768px){
+width: 100%;
+padding: 15px;
+img {
+    width: 40px;
+    height: 40px;
+}
+}
+`;
+export const InfoRight = styled.div`
+width: 502px;
+display: flex;
+flex-direction: column;
+padding: 17px 17px 17px 34px;
+font-family: ${fonts.main};
+h2{
+font-size: 19px;
+font-weight: 400;
+color: ${colors.neutral};
+margin-bottom: 7px;
+}
+p{
+font-size: 17px;
+font-weight: 400;
+line-height: 20px;
+color: ${colors.description};
+}
+@media (max-width: 768px){
+    width: 100%;
+    padding: 17px 17px 17px 0px;
+}
+`;
+export const Articles = styled.div`
+margin-top: 7px;
+width: 503px;
+height: 155px;
+border: 1px solid #4D4D4D;
+border-radius: 11px;
+color: ${colors.description};
+padding: 20px;
+@media (max-width: 768px){
+    width: 288px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+}
+`;
